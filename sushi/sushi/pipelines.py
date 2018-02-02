9568128b40c3baa49408746f6ae90731d0b95741
NIck <1789199491@qq.com>
2018-02-02 09:48:56 +0800

    functions added 1.fix some bugs

--- sushi/sushi/pipelines.py
+++ sushi/sushi/pipelines.py
@@ -12,19 +12,29 @@ import time
 import random
 from sushi.items import *
 import re
+import datetime
+import codecs
 
 urllib.disable_warnings(urllib.exceptions.InsecureRequestWarning)
 
 
 class SushiPipeline(object):
-    img_path_labelled = 'D:\STUDY_SOURCE\sushi\img_1\labeled'
-    img_path_unlabelled = 'D:\STUDY_SOURCE\sushi\img_1\\unlabelled'
-    shop_file_path = 'D:\STUDY_SOURCE\sushi\img_1\shops.jl'
-    img_file_path = 'D:\STUDY_SOURCE\sushi\img_1\images.jl'
+    img_path_labelled = 'D:\STUDY_SOURCE\sushi\img_{0}\labeled\\'
+    img_path_unlabelled = 'D:\STUDY_SOURCE\sushi\img_{0}\\unlabelled\\'
+    shop_file_path = 'D:\STUDY_SOURCE\sushi\img_{0}\\'
+    shop_file_name = 'shops.jl'
+    img_file_path = 'D:\STUDY_SOURCE\sushi\img_{0}\\'
+    img_file_name = 'images.jl'
 
-    seen_urls_path = 'D:\STUDY_SOURCE\sushi\img_1\\visted_url.lines'
+    seen_urls_path = 'D:\STUDY_SOURCE\sushi\\'
+    seen_urls_name = 'visted_url.lines'
 
     SLEEP_TIME = 5
+
+    FLUSH_PARA = 3
+    COUNT = 1
+
+    DATE_STR = ""
 
     user_agent_list = [
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1" \
@@ -51,22 +61,46 @@ class SushiPipeline(object):
         return random.choice(self.user_agent_list)
 
     def open_spider(self, spider):
-        self.shop_file = open(self.shop_file_path, 'w')
-        self.img_file = open(self.img_file_path, 'w')
+        self.open_files()
 
-    def close_spider(self, spider):
+    def close_files(self):
         self.shop_file.close()
         self.img_file.close()
         self.seen_urls_file.close()
 
+    def make_dir(self, File_Path):
+        if not os.path.exists(File_Path):
+            os.makedirs(File_Path)
+
+    def open_files(self):
+        self.make_dir(self.shop_file_path.format(self.DATE_STR))
+        self.make_dir(self.img_file_path.format(self.DATE_STR))
+        self.make_dir(self.seen_urls_path.format(self.DATE_STR))
+
+        self.make_dir(self.img_path_labelled.format(self.DATE_STR))
+        self.make_dir(self.img_path_unlabelled.format(self.DATE_STR))
+        self.shop_file = codecs.open(self.shop_file_path.format(self.DATE_STR) + self.shop_file_name, 'w+', encoding='utf8')
+        self.img_file = codecs.open(self.img_file_path.format(self.DATE_STR) + self.img_file_name, 'w+', encoding='utf8')
+        self.seen_urls_file = codecs.open(self.seen_urls_path + self.seen_urls_name, 'a+', encoding='utf8')
+
+    def close_spider(self, spider):
+        self.close_files()
+
     def __init__(self):
-        self.seen_urls_file = open(self.seen_urls_path, 'w')
+        self.DATE_STR = datetime.datetime.now().strftime('%Y-%m-%d')
         self.urls_seen = set()
+        self.open_files()
+        self.seen_urls_file.seek(0)
+        for i, url in enumerate(self.seen_urls_file.readlines()):
+            self.urls_seen.add(url.replace("\n", ""))
+        print('Load '+str(len(self.urls_seen))+' visited urls...')
+
 
     # Handle Comment here, return item
     def handleShop(self, item, spider):
         line = json.dumps(dict(item)) + "\n"
         self.shop_file.write(line)
+        self.COUNT = 1 + self.COUNT
 
     # Handle profile here, return item
     def handleImage(self, item, spider):
@@ -76,20 +110,36 @@ class SushiPipeline(object):
         http = urllib.PoolManager(headers={'User-Agent': self.getRandomHeader()})
         image_urls = item['image_urls']
         for image_url in image_urls:
+            self.COUNT = 1 + self.COUNT
+            tmp_url = image_url['img_url']
             time.sleep(random.randint(1, self.SLEEP_TIME))
-            res = http.request('GET', image_url['img_url'])
+            res = http.request('GET', tmp_url)
             # print(image_url['img_url'] + '-' + str(res.status))
             sushi_name = self.validate_title(image_url['name'])
             # sushi_hash = str(hash(image_url['img_url']))
-            uuid_suffix = image_url['img_url'][-12:]
-            file_name = os.path.join(self.img_path_labelled, sushi_name) + '-' + uuid_suffix
+            uuid_suffix = tmp_url[-12:]
+            file_name = os.path.join(self.img_path_labelled.format(self.DATE_STR), sushi_name) + '-' + uuid_suffix
             if not self.is_labelled(sushi_name):
-                file_name = os.path.join(self.img_path_unlabelled, sushi_name) + '-' + uuid_suffix
+                file_name = os.path.join(self.img_path_unlabelled.format(self.DATE_STR), sushi_name) + '-' + uuid_suffix
             with open(file_name, 'wb') as fp:
                 fp.write(res.data)
                 print('written file:'+file_name)
+            self.urls_seen.add(tmp_url+ "\n")
+            self.seen_urls_file.write(tmp_url + "\n")
 
     def process_item(self, item, spider):
+        self.COUNT = 1 + self.COUNT
+        new_date_str = datetime.datetime.now().strftime('%Y-%m-%d')
+        if self.COUNT % self.FLUSH_PARA == 0:
+            self.close_files()
+            self.open_files()
+            print('Reset file success!')
+        if new_date_str != self.DATE_STR:
+            self.DATE_STR = new_date_str
+            self.close_files()
+            self.open_files()
+            print('Reset file success!')
+
         if item['url'] in self.urls_seen:
             raise DropItem("Duplicate url found in item: %s" % item)
         else:
@@ -101,13 +151,14 @@ class SushiPipeline(object):
             except Exception as e:
                 print('pipeline错误.' + str(e))
             else:
-                self.urls_seen.add(item['url'])
-                self.seen_urls_file.write(item['url'])
+                self.urls_seen.add(item['url'] + "\n")
+                self.seen_urls_file.write(item['url'] + "\n")
+                return
 
     def validate_title(self, title):
         rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
         new_title = re.sub(rstr, " ", title)  # 替换为下划线
-        return new_title
+        return new_title.replace("\r\n", " ").replace("\n", " ").replace("/", " ")
 
     def is_labelled(self, title):
         arr = re.split('[-]', title)
